2b03b53a62df832d8aac60cd13ee43ce829fa807
aboaisheh <aboaishehmohammed@gmail.com>
2024-11-27 15:43:08 +0300

    make service creation optional

--- src/Commands/MakeRepositoryCommand.php
+++ src/Commands/MakeRepositoryCommand.php
@@ -7,7 +7,7 @@ use Aboaisheh\LaravelRepository\Generators\ServiceGenerator;
 
 class MakeRepositoryCommand extends Command
 {
-    protected $signature = 'make:repository {name}';
+    protected $signature = 'make:repository {name} {--service}';
     protected $description = 'Create a new repository and its corresponding service';
 
     public function handle()
@@ -17,8 +17,11 @@ class MakeRepositoryCommand extends Command
         $servicePath = app_path("Services/{$name}Service.php");
 
         RepositoryGenerator::generate($name, $repositoryPath);
-        ServiceGenerator::generate($name, $servicePath);
+        $this->info("Repository '{$name}Repository' created successfully!");
 
-        $this->info("Repository and Service created successfully!");
+        if ($this->option('service')) {
+            ServiceGenerator::generate($name, $servicePath);
+            $this->info("Service '{$name}Service' created successfully!");
+        }
     }
 }
